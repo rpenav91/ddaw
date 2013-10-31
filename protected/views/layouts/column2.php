f72f12387c4ea13573906c1e776c5d2b27334a6a
René Peña <rpenav91@hotmail.com>
2013-10-31 11:26:50 -0600

    Primer Commit
    primer commit

---
+++ protected/views/layouts/column2.php
@@ -0,0 +1,24 @@
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
+<div class="row-fluid">
+	<div class="span9">
+		<?php echo $content; ?>
+	</div>
+		
+	<div class="span3">
+		<?php
+			$this->beginWidget('bootstrap.widgets.TbMenu', array(
+				'type'=>'list',
+				'htmlOptions'=>array("class"=>"well"),
+				'stacked'=>false,
+				'items'=>$this->menu,
+			));				
+			$this->endWidget();
+			
+		?>
+
+	</div>
+</div> <!--row-fluid del column2-->
+
+
+<?php $this->endContent(); ?>
